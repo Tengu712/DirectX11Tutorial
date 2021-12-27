5b998fa695ccf20d25e3219c177e18394d3452d0
Tengu712 <65766435+Tengu712@users.noreply.github.com>
2021-12-28 01:10:54 +0900

    window class

--- 03_windowclass/Main.cpp
+++ 03_windowclass/Main.cpp
@@ -4,25 +4,38 @@
 
 HWND g_hWnd;
 
+// Define window procedure for window class
+LRESULT WINAPI WndProc(HWND h_wnd, unsigned int msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+    }
+    return DefWindowProcW(h_wnd, msg, wParam, lParam);
+}
+
 // Define function to create and show a window
 bool CreateShowWindow(HINSTANCE h_inst, long width, long height, LPCSTR name_window, LPCSTR name_window_class,
     int cmd_show, bool windowed) {
-    // Constants
     const DWORD kDwStyle = windowed ? WS_OVERLAPPED | WS_SYSMENU | WS_MINIMIZEBOX : WS_POPUP;
     const int kCmdShow = windowed ? cmd_show : SW_SHOWMAXIMIZED;
 
-    // Adjust window size with window border
+    // Create window class structure
+    WNDCLASSEXA wcex = {sizeof(WNDCLASSEXA), CS_CLASSDC, WndProc, 0L, 0L, h_inst, nullptr, nullptr, nullptr, nullptr,
+        name_window_class, nullptr};
+    // Register it
+    if (!RegisterClassExA(&wcex))
+        return false;
+
     RECT rect;
     rect = {0, 0, width, height};
     AdjustWindowRectEx(&rect, kDwStyle, false, 0);
 
-    // Create window and get the window handle
-    g_hWnd = CreateWindowExA(0, "COMBOBOX", name_window, kDwStyle, CW_USEDEFAULT, CW_USEDEFAULT,
+    g_hWnd = CreateWindowExA(0, name_window_class, name_window, kDwStyle, CW_USEDEFAULT, CW_USEDEFAULT,
         rect.right - rect.left, rect.bottom - rect.top, nullptr, nullptr, h_inst, nullptr);
     if (!g_hWnd)
         return false;
 
-    // Visualize window
     ShowWindow(g_hWnd, kCmdShow);
     UpdateWindow(g_hWnd);
     return true;
@@ -31,7 +44,5 @@ bool CreateShowWindow(HINSTANCE h_inst, long width, long height, LPCSTR name_win
 int WINAPI WinMain(HINSTANCE h_inst, HINSTANCE h_pinst, LPSTR p_cmd, int cmd_show) {
     bool flg = true;
     flg = flg && CreateShowWindow(h_inst, 1280, 720, "Window name", "Window class name", cmd_show, false);
-    while(true) {
-    }
     return 0;
 }
